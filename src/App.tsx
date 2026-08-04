f03ecdb1653d31ee794d42c2ae2765f0a63e8ed6
Thomas Maloney <thomasjmaloney99@gmail.com>
2026-08-03 22:49:09 -0500

    added garbage comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ChantEditor } from './editor/components/ChantEditor';
 import { exampleDocument } from './editor/domain/example-document';
 import './App.css';
 
+// Blah
 function App() {
   return <ChantEditor document={exampleDocument} />;
 }
